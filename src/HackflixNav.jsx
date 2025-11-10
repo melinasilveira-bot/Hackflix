55cab22bb64b797e6bf33ce6354858296bab89ff
Neitan501 <nahuelesketit@gmail.com>
2025-11-10 18:17:41 -0300

    Agregar página /buscar con búsqueda en TMDB + useInput hook
    Implementación de /buscar, búsqueda en tiempo real con TMDB, diseño con grid estilo Netflix, mejoras visuales, integración con modal y creación de hook personalizado useInput.

--- src/HackflixNav.jsx
+++ src/HackflixNav.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { Navbar, Container, Button } from "react-bootstrap";
-//  asume Font Awesome cargado en index.html
+import { Link } from "react-router-dom"; // IMPORTAR Link
 
 const HackflixNav = () => {
-   return (
-      <Navbar className="navbar" fixed="top" expand="lg">
-         <Container fluid>
-            {/* Logo Hackflix */}
-            <Navbar.Brand href="#" className="logo">
-               HACKFLIX
-            </Navbar.Brand>
+  return (
+    <Navbar className="navbar" fixed="top" expand="lg">
+      <Container fluid>
+        {/* Logo Hackflix */}
+        <Navbar.Brand as={Link} to="/" className="logo">
+          HACKFLIX
+        </Navbar.Brand>
 
-            {/* Botón Home */}
-            <Button variant="outline-light" className="nav-button">
-               {/* Icono de Font Awesome */}
-               <i className="fas fa-home me-2"></i> Home
-            </Button>
-         </Container>
-      </Navbar>
-   );
+        {/* Botón Home */}
+        <Button as={Link} to="/" variant="outline-light" className="nav-button">
+          <i className="fas fa-home me-2"></i> Home
+        </Button>
+
+        {/* Botón Buscar */}
+        <Button
+          as={Link}
+          to="/buscar"
+          variant="outline-light"
+          className="nav-button ms-2"
+        >
+          <i className="fas fa-search me-2"></i> Buscar
+        </Button>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default HackflixNav;
